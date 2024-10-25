058edea5cd7110a456d81768e58e195756a79244
gabbogaitan <103861426+gabbogaitan@users.noreply.github.com>
2024-10-24 23:01:25 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function sendmessages() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+  
+    // Enviar mensaje por WhatsApp
+    var whatsappURL = 'https://wa.me/+5491123909541?text=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
+    window.open(whatsappURL, '_blank');
+  
+    // Enviar mensaje por Email
+    var mailtoLink = 'mailto:gaitan.gabriel@hotmail.com?subject=' + encodeURIComponent('Mensaje de ' + name) + '&body=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
+    window.open(mailtoLink, '_blank');
+  }
