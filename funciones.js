2acc692ee253ff250be7fe9039d57b7828bbe6bb
gabbogaitan <103861426+gabbogaitan@users.noreply.github.com>
2024-11-05 20:30:41 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -4,8 +4,8 @@ function sendmessages() {
     var message = document.getElementById('message').value;
   
     // Enviar mensaje por WhatsApp
-    var whatsappURL = 'https://wa.me/+5491123909541?text=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
-    window.open(whatsappURL, '_blank');
+    //var whatsappURL = 'https://wa.me/+5491123909541?text=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
+    //window.open(whatsappURL, '_blank');
   
     // Enviar mensaje por Email
     var mailtoLink = 'mailto:gaitan.gabriel@hotmail.com?subject=' + encodeURIComponent('Mensaje de ' + name) + '&body=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
